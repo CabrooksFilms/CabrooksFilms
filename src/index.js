03c8421c92848bf958f81622d4b2397d2ae358c0
Mark Rivera <Mrivera1991@gmail.com>
2019-04-29 22:13:49 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const Glide = require('@glidejs/glide');
+
+function createSlide() {
+	// Create Slideshow and mount to DOM
+	const slides = new Glide('.glide', {
+		type: 'carousel',
+		perView: 3,
+		autoplay: 10000,
+		peek: 50,
+		breakpoints: {
+			600: {
+				perView: 1,
+				peek: 0,
+			}
+		}
+	});	
+	slides.mount();
+}
+
+window.addEventListener('DOMContentLoaded', e => {
+	//Check if page is index.html
+	if (e.target.location.pathname !== '/index.html' || e.target.location.pathname !== '/') {
+		return
+	} 
+	else {
+		createSlide();
+	}
+});
